967a4ae53a1520fa3e14b772eb6c07a2ee0b325c
Daniel Georgiev <georgiev.daniel@outlook.com>
2025-12-03 21:55:25 +0200

    implement getTeachers handler

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"strconv"
+	"strings"
+	"sync"
 
 	"github.com/georgiev098/golang-basic-crud-api/internal/api/middleware"
 	"github.com/georgiev098/golang-basic-crud-api/internal/middlewares"
@@ -13,12 +16,93 @@ import (
 
 const PORT = "3000"
 
+type Teacher struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Class     string
+	Subject   string
+}
+
+var (
+	teachers = make(map[int]Teacher)
+	mutex    = &sync.Mutex{}
+	nextId   = 1
+)
+
+func init() {
+	teachers[nextId] = Teacher{
+		ID:        nextId,
+		FirstName: "John",
+		LastName:  "Doe",
+		Class:     "9A",
+		Subject:   "Math",
+	}
+	nextId++
+	teachers[nextId] = Teacher{
+		ID:        nextId,
+		FirstName: "Jane",
+		LastName:  "Doe",
+		Class:     "10A",
+		Subject:   "Algebra",
+	}
+
+}
+
+func getTeaches(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/teachers")
+	idStr := strings.TrimSuffix(path, "/")
+
+	if idStr == "" {
+		firstName := r.URL.Query().Get("first_name")
+		lastName := r.URL.Query().Get("last_name")
+		teacherList := make([]Teacher, 0, len(teachers))
+
+		for _, v := range teachers {
+			if (firstName == "" || v.FirstName == firstName) && (lastName == "" || v.LastName == lastName) {
+				teacherList = append(teacherList, v)
+			}
+		}
+
+		resp := struct {
+			Status string    `json:"status"`
+			Count  int       `json:"count"`
+			Data   []Teacher `json:"data"`
+		}{
+			Status: "success",
+			Count:  len(teacherList),
+			Data:   teacherList,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
+	}
+
+	// handle path param
+	idNum, err := strconv.Atoi(idStr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	teacher, exists := teachers[idNum]
+	if !exists {
+		http.Error(w, "Teacher not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(teacher)
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the root route"))
 }
 
 func teachersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from the teachers route"))
+	switch r.Method {
+	case http.MethodGet:
+		getTeaches(w, r)
+	}
 }
 
 func studentsHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,7 +134,7 @@ func main() {
 		MinVersion: tls.VersionTLS12,
 	}
 
-	rl := middlewares.NewRateLimiter(5, time.Minute)
+	// rl := middlewares.NewRateLimiter(5, time.Minute)
 
 	hppOptions := middlewares.HPPOptions{
 		CheckQuery:                  true,
@@ -59,7 +143,7 @@ func main() {
 		Whitelist:                   []string{"sortBy", "sortOrder", "name", "age", "class"},
 	}
 
-	secureMux := middlewares.Hpp(hppOptions)(rl.Middleware(middleware.Compression(mux)))
+	secureMux := applyMiddlewares(mux, middlewares.Hpp(hppOptions), middleware.Compression)
 
 	server := &http.Server{
 		Addr:      ":" + PORT,
@@ -73,4 +157,13 @@ func main() {
 		log.Fatal("Error starting the server: ", err)
 	}
 
+}
+
+type Middleware func(http.Handler) http.Handler
+
+func applyMiddlewares(handler http.Handler, middlewares ...Middleware) http.Handler {
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+	return handler
 }
